8c99a7c808c037a6d1248e40fbf990f0dbca674d
EmmanuelBarboza <barbozaprogra18@gmail.com>
2025-03-13 19:23:20 -0600

    Actualizacion general
    Añadidas nuevas secciones, barra de navegación y experiencias

--- script.js
+++ script.js
@@ -22,4 +22,19 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+
+        // JavaScript para el desplazamiento suave
+    document.querySelectorAll('.navbar-list a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+
+
 });
